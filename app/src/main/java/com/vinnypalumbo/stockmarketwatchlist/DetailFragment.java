c27f2b82c1a5ce4b391537ae299ea32abec4c75f
Vinny <vin_palumbo@hotmail.com>
2016-10-16 13:56:56 -0400

    Add DetailActivity and DetailFragment

---
+++ app/src/main/java/com/vinnypalumbo/stockmarketwatchlist/DetailFragment.java
@@ -0,0 +1,25 @@
+package com.vinnypalumbo.stockmarketwatchlist;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Vincent on 2016-10-14.
+ */
+
+public class DetailFragment extends Fragment {
+
+    public DetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
