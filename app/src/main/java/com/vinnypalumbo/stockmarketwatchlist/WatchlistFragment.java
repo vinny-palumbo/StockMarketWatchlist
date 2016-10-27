793a10fc5634f698236608557bfd4beb55431d6b
Vinny <vin_palumbo@hotmail.com>
2016-10-26 22:01:02 -0400

    Add Refresh button to Main screen menu

--- app/src/main/java/com/vinnypalumbo/stockmarketwatchlist/WatchlistFragment.java
+++ app/src/main/java/com/vinnypalumbo/stockmarketwatchlist/WatchlistFragment.java
@@ -3,6 +3,9 @@ package com.vinnypalumbo.stockmarketwatchlist;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +30,32 @@ public class WatchlistFragment extends Fragment {
         super.onStart();
         FetchStocksTask fetchTask = new FetchStocksTask(getActivity(), mWatchlistAdapter);
         fetchTask.execute();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.fragment_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            FetchStocksTask fetchTask = new FetchStocksTask(getActivity(), mWatchlistAdapter);
+            fetchTask.execute();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
